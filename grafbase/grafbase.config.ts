552436f8e45a8cf8f6aa66bc614aac77cb9e3ad9
reobest <sabdelrahman071@gmail.com>
2024-06-29 15:43:01 +0300

    first commi

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,25 +1,31 @@
-import { g, config } from '@grafbase/sdk'
 
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 100 }),
+import { graph, config } from '@grafbase/sdk'
+
+const g = graph.Standalone()
+
+// Define the User type
+const User = g.type('User', {
+  name: g.string().length({min: 3 , max : 100}),
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().length({ min: 2, max: 1000 }).optional(),
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(), 
-  projects: g.list(g.relation(() => Project)).optional(),
+  // projects: g.list(relation('Project')).optional(), 
 })
 
-const Project = g.model('Project', {
+// Define the Project type
+const Project = g.type('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
   image: g.url(),
   liveSiteUrl: g.url(), 
   githubUrl: g.url(), 
-  category: g.string().search(),
-  createdBy: g.relation(() => User),
+  category: g.string(),
+  // createdBy: g.relation('User'), // Use type name as a string for relation
 })
 
+// Export the configuration
 export default config({
   schema: g,
   auth: {
