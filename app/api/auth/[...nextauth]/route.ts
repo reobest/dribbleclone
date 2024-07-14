b0ce61f570bf1af0c20356771a83a26df5de7321
reobest <sabdelrahman071@gmail.com>
2024-07-14 19:33:01 +0300

    final commit?

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import { authOptions } from "@/lib/session"
 import NextAuth from "next-auth"
-
-const handler = NextAuth(authOptions)
+import { NextApiRequest , NextApiResponse } from "next"
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    return NextAuth(req, res, authOptions);
+  };
 
 export { handler as GET, handler as POST }
